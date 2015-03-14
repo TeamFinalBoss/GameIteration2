0708d684006513c2faefc65cdb8c29e127de610a
jasontowens <jasontowens94@gmail.com>
2015-03-14 18:00:21 -0400

    Test commit

---
+++ TFBGame/src/model/entities/Direction.java
@@ -0,0 +1,17 @@
+package src.model.entities;
+
+/**
+ * This enum is used for passing direction between classes that shouldn't
+ * know each other's internal logic
+ * @author Jason Owens
+ */
+public enum Direction {
+    NORTH,
+    NORTHEAST,
+    EAST,
+    SOUTHEAST,
+    SOUTH,
+    SOUTHWEST,
+    WEST,
+    NORTHWEST 
+}
--- TFBGame/src/model/entities/Entity.java
+++ TFBGame/src/model/entities/Entity.java
@@ -1,10 +1,13 @@
 package model.entities;
 
+import src.model.entities.Direction;
+
 /** The class Entity defines a common type for all entities (beings) in the game. 
  * @author Matthew Kroeze
  * @version 1.0.0 2015-03-14
  */
 public abstract class Entity{
+        private Direction myDirection;
 	private Inventory myInventory;
 	private Occupation myOccupation;
 }
