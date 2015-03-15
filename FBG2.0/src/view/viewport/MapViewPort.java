f773000a9582e2173447869e212b2fc3f3cffd73
Chris Moscoso <christophermoscoso@gmail.com>
2015-03-15 10:43:59 -0400

    Temporary MapViewport

--- FBG2.0/src/view/viewport/MapViewPort.java
+++ FBG2.0/src/view/viewport/MapViewPort.java
@@ -1,5 +1,7 @@
 package view.viewport;
 
+import java.awt.Color;
+import java.awt.Font;
 import java.awt.Graphics;
 import java.awt.image.BufferedImage;
 import java.io.File;
@@ -9,6 +11,7 @@ import java.util.Observer;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.imageio.ImageIO;
+import model.director.GameDirector;
 import model.map.GameMap;
 import model.map.tile.Tile;
 
@@ -23,17 +26,27 @@ public class MapViewPort implements ViewPort, Observer {
 
     @Override
     public void draw(Graphics g) {
+        System.out.println("what's the hold up");
         for (int i = 0; i < widthInTiles; i++) {
             for (int j = 0; j < heightInTiles; j++) {
-                try {
+                /*try {
                     //Draw Tiles
                     //TODO: Make it so it doesnt just draw grass
                     BufferedImage img = ImageIO.read(new File("src/resources/grass.jpg"));
-                    //g.drawImage(img, i * 64, j * 64, 64, 64, null);
+                    g.drawImage(img, i * 64, j * 64, 64, 64, null);
                     g.drawRect(i*64, j*64, 32, 32);
-                } catch (IOException ex) {}
+                    
+                    
+                } catch (IOException ex) {}*/
             }
         }
+        String temp = "EPIC GAME SCENE HERE ;P";
+        g.setFont(new Font(g.getFont().getFamily(), Font.PLAIN, 30));
+        g.setColor(Color.green);
+        int stringWidth = g.getFontMetrics().stringWidth(temp);
+        int x = GameDirector.getSize().width/2;
+        int y = GameDirector.getSize().height/2;
+        g.drawString(temp, x - stringWidth / 2, y);
     }
 
     @Override
