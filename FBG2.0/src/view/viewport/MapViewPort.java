fa0e9fa27a3c48b0dc76a7727802c3d02f0eb7be
Chris Moscoso <christophermoscoso@gmail.com>
2015-03-15 11:38:50 -0400

    Shows grass

--- FBG2.0/src/view/viewport/MapViewPort.java
+++ FBG2.0/src/view/viewport/MapViewPort.java
@@ -3,8 +3,14 @@ package view.viewport;
 import java.awt.Color;
 import java.awt.Font;
 import java.awt.Graphics;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 import java.util.Observable;
 import java.util.Observer;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.imageio.ImageIO;
 import model.director.GameDirector;
 import model.map.tile.Tile;
 
@@ -16,29 +22,30 @@ public class MapViewPort implements ViewPort, Observer {
 
     Tile[][] tiles;
     int widthInTiles = 0, heightInTiles = 0;
+    BufferedImage grass;
 
+    public MapViewPort(){
+        try {
+            grass = ImageIO.read(new File("src/resources/grass.jpg"));
+        } catch (IOException ex) {
+            
+        }
+    }
+    
     @Override
     public void draw(Graphics g) {
         for (int i = 0; i < widthInTiles; i++) {
             for (int j = 0; j < heightInTiles; j++) {
-                /*try {
                     //Draw Tiles
                     //TODO: Make it so it doesnt just draw grass
-                    BufferedImage img = ImageIO.read(new File("src/resources/grass.jpg"));
-                    g.drawImage(img, i * 64, j * 64, 64, 64, null);
-                    g.drawRect(i*64, j*64, 32, 32);
+                    
+                    g.drawImage(grass, i * 64, j * 64, 64, 64, null);
                     
                     
-                } catch (IOException ex) {}*/
+                    
+                
             }
         }
-        String temp = "EPIC GAME SCENE HERE ;P";
-        g.setFont(new Font(g.getFont().getFamily(), Font.PLAIN, 30));
-        g.setColor(Color.green);
-        int stringWidth = g.getFontMetrics().stringWidth(temp);
-        int x = GameDirector.getSize().width/2;
-        int y = GameDirector.getSize().height/2;
-        g.drawString(temp, x - stringWidth / 2, y);
     }
 
     @Override
@@ -46,6 +53,12 @@ public class MapViewPort implements ViewPort, Observer {
         tiles = (Tile [][]) arg;
         widthInTiles = tiles.length;
         heightInTiles = tiles[0].length;
+    }
+
+    private static class pubilc {
+
+        public pubilc() {
+        }
     }
 
 }
