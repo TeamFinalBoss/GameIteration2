8a0f4b60c6aa0b8b6123b4700a50203469ee5014
jasontowens <jasontowens94@gmail.com>
2015-03-16 17:27:41 -0400

    Added Ability subclasses

---
+++ FBG2.0/src/model/map/CombatCoordinator.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model.map;
+
+import java.util.ArrayList;
+import model.ability.Effect;
+import model.map.pair.CoordinatePair;
+
+/**
+ * Facilitates projectiles and abilities
+ * @author Jason Owens
+ */
+public class CombatCoordinator {
+    
+    CombatCoordinator(){
+        
+    }
+    
+    public void AttemptAffectEntities(ArrayList<CoordinatePair> listOfCPs, Effect effect){
+        for(CoordinatePair CP: listOfCPs){
+            
+        }
+    }
+    public boolean AttemptAffectEntity(CoordinatePair CP){
+        Entity entityToAffect = entityInCoordinatePair(CP)
+        if(entityToAffect != null){
+            
+        }
+    }
+    
+}
