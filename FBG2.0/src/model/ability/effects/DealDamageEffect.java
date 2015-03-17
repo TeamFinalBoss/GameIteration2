338024700b2f1b37c192a5dbcaa84c37ad6db1a2
jasontowens <jasontowens94@gmail.com>
2015-03-16 20:56:47 -0400

    Added TemporaryEffect

--- FBG2.0/src/model/ability/effects/DealDamageEffect.java
+++ FBG2.0/src/model/ability/effects/DealDamageEffect.java
@@ -4,6 +4,7 @@ import model.entity.Entity;
 import model.stats.PlayerStats;
 
 /**
+ * TODO: add to this class when stat changes are implemented
  * Is an example OneTimeEffect class. Will probably be used by basic attacks.
  * @author Jason Owens
  */
--- FBG2.0/src/model/ability/effects/TemporaryEffect.java
+++ FBG2.0/src/model/ability/effects/TemporaryEffect.java
@@ -1,13 +1,10 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package model.ability.effects;
 
 import model.entity.Entity;
 
+
 /**
+ * TODO: add to this class when stat changes are implemented
  * For Effects that apply changes and then revert them after a certain amount of time
  * @author Jason owens
  */
