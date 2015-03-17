92c0d764b59d89e58a47dc6dc67504c818a72c3c
jasontowens <jasontowens94@gmail.com>
2015-03-17 14:42:14 -0400

    Revert "Revert "Uhhh idk""
    This reverts commit f718e1c88ef901c8cc4291cbc8e05120530fc742, reversing
    changes made to 1c299b25b3056b3e8d5f149a65d17c3d8d36d9c9.

--- FBG2.0/src/model/entity/Armory.java
+++ FBG2.0/src/model/entity/Armory.java
@@ -5,54 +5,73 @@ import model.item.EquipSlot;
 import java.util.HashMap;
 import java.util.Collections;
 import java.util.Map;
-/** An Armory object is a component of an entity's inventory and holds its equipped items.
+/** 
+ * An Armory object is a component of an entity's inventory and holds its equipped items.
  * @author Matthew Kroeze
  * @version 1.0.0 2015-03-15
  */
 public class Armory {
 	private HashMap<EquipSlot,Equipable> contents;
+	private Entity owner;
 	
 	/* -------------------- CONSTRUCTORS -------------------- */
 	
-	/** Creates an empty <code>Armory</code> */
-	public Armory(){
+	/** Creates an empty Armory */
+	public Armory(Entity owner){
+		this.owner = owner;
 		contents = new HashMap<EquipSlot,Equipable>();
 	}
 	
 	/* -------------------- ACCESSORS -------------------- */
 	
-	/** Returns the contents of the armory
-	 * @return a <code>Map</code> object with all <code>Equipable</code> objects
-	 * 		   in the armory as values and their <code>EquipSlot</code>s as keys.
+	/** 
+	 * Returns the contents of the armory.
+	 * @return an immutable Map object with all Equipable objects
+	 * 		   in the armory as values and their EquipSlots as keys.
 	 */
 	public Map<EquipSlot,Equipable> contents(){
 		return Collections.unmodifiableMap(contents);
 	}
 	
 	/* -------------------- MUTATORS -------------------- */ 
-	/** Equips the passed <code>Equipable</code> object. Is NOT responsible for ensuring
-	 *  that the effects and stat changes of the equipment are applied. Is NOT responsible 
-	 *  for verifying that the equipee meets the requirements of the equipment.
-	 * @param newest the <code>Equipable</code> object to be equipped
+	/** 
+	 * Equips the passed Equipable object if the owner of the armory meets that Equipable's equip requirements. 
+	 * @param newest the Equipable object to be equipped.
+	 * @return <code>null</code> if the new equipment's slot was empty.
+	 * 		   the previously occupying Equipable of that slot if the new equipment's slot was filled.
+	 * @throws IllegalArgumentException if the owner of the armory does not meet the new equipment's requirements
 	 */
-	public void equip(Equipable newest){
+	public Equipable equip(Equipable newest){
+		Equipable old = null;
+		//fail if owner of armory does not meet the equipment's requirements
+		if(!newest.meetsRequirements(owner)) throw new IllegalArgumentException("Equip failed: Owner does not meet equipment requirements");
+		//unequip the slot if something is already occupying it
 		if(contents.containsKey(newest.slot())){
-			unequip(newest.slot());
+			old = unequip(newest.slot());
 		}
+		//insert the new equipment into the armory
 		contents.put(newest.slot(), newest);
+		//inform the equipable that it has been equipped so that it may perform its on-equip effects
+		newest.onEquip(owner);
+		//return whatever equipment was removed
+		return old;
 	}
 	
-	/** Unequips and returns the equipment in the specified slot. Is NOT responsible for
-	 *  undoing the stat changes of that equipment
-	 * @param slot the <code>EquipSlot</code> of the <code>Equipable</code> item to be unequipped
-	 * @return the <code>Equipable</code> item that was unequipped
-	 * @throws <code>IllegalArgumentException</code> if no equipment occupied the specified slot
+	/** 
+	 * Unequips and returns the equipment in the specified slot if that slot is occupied.
+	 * @param slot the EquipSlot of the Equipable item to be unequipped.
+	 * @return the Equipable item that was unequipped.
+	 * @throws IllegalArgumentException if no equipment occupied the specified slot.
 	 */
 	public Equipable unequip(EquipSlot slot){
+		//verify that the slot is occupied
 		if(!contents.containsKey(slot)){
 			throw new IllegalArgumentException("There is no item to be unequipped in the " + slot + " slot");
 		}
-		return contents.remove(slot);
+		//inform the equipment that it is being unequipped and return it
+		Equipable old = contents.remove(slot);
+		old.onUnequip(owner);
+		return old;
 	}
 
 }
