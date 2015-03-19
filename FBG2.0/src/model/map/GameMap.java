41e4091ddd7b7f712872b2fcd7a8d7790e76ae29
mickeyjk <micohen13@gmail.com>
2015-03-19 13:45:22 -0400

    Revert "added removeX for Trap, AE, & Item for GameMap"
    This reverts commit f37b6c5459f1214e5b9cf0e69012eb73b88cc6fc.

--- FBG2.0/src/model/map/GameMap.java
+++ FBG2.0/src/model/map/GameMap.java
@@ -106,7 +106,7 @@ public class GameMap extends Observable {
      * the provided CoordinatePair is not present, a runtime exception is thrown 
      * 
      * @author Michael Cohen
-     * @param CP location of the entity which is queried
+     * @param CP location of the object which is queried
      * @return Entity that was removed from the map
      */
     public Entity removeEntity(CoordinatePair CP){
@@ -131,32 +131,6 @@ public class GameMap extends Observable {
             return false;
         }
     }
-    
-    /**
-     * This method attempts to remove the provided item
-     * from the Item Locations collection and returns
-     * whether or not that removal was successful
-     * 
-     * @author Michael Cohen
-     * @param i the item to be removed
-     * @return true if item was removed, else false
-     */
-    public final boolean removeItem(Item i){
-    	return this.items.remove(i);
-    }
-    
-    /**
-     * Removes and returns the item from the map. If 
-     * the provided CoordinatePair is not present, a runtime exception is thrown 
-     * 
-     * @author Michael Cohen
-     * @param CP location of the item which is queried
-     * @return Item that was removed from the map
-     */
-    public Item removeItem(CoordinatePair CP){
-    	return this.items.remove(CP);
-    }
-    
     /**
      * Adds an area effect to the map at the valid coordinate pair specified if not
      * already occupied by another area effect
@@ -174,32 +148,6 @@ public class GameMap extends Observable {
     	}
     	else return false;
     }    
-    
-    /**
-     * This method attempts to remove the provided area effect
-     * from the AreaEffect Locations collection and returns
-     * whether or not that removal was successful
-     * 
-     * @author Michael Cohen
-     * @param effect to be removed
-     * @return true if effect was removed, else false
-     */
-    public final boolean removeAreaEffect(AreaEffect effect){
-    	return this.effects.remove(effect);
-    }
-    
-    /**
-     * Removes and returns the area effect from the map. If 
-     * the provided CoordinatePair is not present, a runtime exception is thrown 
-     * 
-     * @author Michael Cohen
-     * @param CP location of the effect which is queried
-     * @return AreaEffect that was removed from the map
-     */
-    public AreaEffect removeAreaEffect(CoordinatePair CP){
-    	return this.effects.remove(CP);
-    }
-    
     /**
      * Adds a trap to the map at the valid coordinate pair specified if not
      * already occupied by another trap.
@@ -218,32 +166,6 @@ public class GameMap extends Observable {
             return false;
         }
     }
-    
-    /**
-     * This method attempts to remove the provided trap
-     * from the Trap Locations collection and returns
-     * whether or not that removal was successful
-     * 
-     * @author Michael Cohen
-     * @param t trap to be removed
-     * @return true if trap was removed, else false
-     */
-    public final boolean removeTrap(Trap t){
-    	return this.traps.remove(t);
-    }
-    
-    /**
-     * Removes and returns the trap from the map. If 
-     * the provided CoordinatePair is not present, a runtime exception is thrown 
-     * 
-     * @author Michael Cohen
-     * @param CP location of the trap which is queried
-     * @return Trap that was removed from the map
-     */
-    public Trap removeTrao(CoordinatePair CP){
-    	return this.traps.remove(CP);
-    }
-    
     /**
      * Tests if the coordinate pair is non negative and not beyond the size of
      * the map
