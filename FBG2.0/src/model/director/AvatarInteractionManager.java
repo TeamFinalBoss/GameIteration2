ff0275d518afe6ca2d7e88f9aa94e93944441a44
jasontowens <jasontowens94@gmail.com>
2015-03-20 01:50:12 -0400

    AvatarInteractionManager inventory methods added

--- FBG2.0/src/model/director/AvatarInteractionManager.java
+++ FBG2.0/src/model/director/AvatarInteractionManager.java
@@ -13,6 +13,9 @@ public class AvatarInteractionManager {
     private Avatar avatar;
     private ActiveMapManager AMM;
     
+    private int currentSlotInSack;
+    private int currentSlotInArmory;
+    
  
     public AvatarInteractionManager(){
        AMM = ActiveMapManager.getInstance();
@@ -21,12 +24,34 @@ public class AvatarInteractionManager {
         this.avatar = avatar;
     }
     
+    
+    public int getCurrentSlotInSack(){
+        return currentSlotInSack;
+    }
+    
+    public int getCurrentSlotInArmory){
+        return currentSlotInArmory;
+    }
+    
+    
+    /**
+     * Moves the avatar
+     * @param dir the direction to move
+     * @return whether or not the movement is valid
+     */
     public boolean moveAvatar(Direction dir){
         return AMM.requestMovement(avatar, dir);
     }
     
+    /**
+    * Should return the number of slots used in the sack including blank ones, if I understand
+    * ArrayLists correctly. So, if you have 10 items and you remove the 5th one, it will still
+    * return 10, but then if you add a new item it will still be 10 because the new item is 
+    * going into the old, now empty slot.
+    * @author Jason Owens
+    */
     public int getSackSize(){
-        avatar.getSackSize();
+        return avatar.getSackSize();
     }
     
     
