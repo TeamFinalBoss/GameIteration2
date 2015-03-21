b771d67e530d887317fffb46a7a0842d22d4cda7
jasontowens <jasontowens94@gmail.com>
2015-03-21 18:54:25 -0400

    Entity's direction should now update on move

--- FBG2.0/src/model/map/GameMap.java
+++ FBG2.0/src/model/map/GameMap.java
@@ -431,6 +431,7 @@ public class GameMap extends Observable {
         if(MV.canTraverse(e.getMotionType(), getItemAtCoordinate(desiredLocation), getTileAtCoordinate(desiredLocation).getMotionType())){ 
             MC.moveEntity(e, desiredLocation, getAreaEffectAtCoordinate(desiredLocation), getItemAtCoordinate(desiredLocation),
                     getSwitcherAtCoordinate(desiredLocation), getTrapAtCoordinate(desiredLocation));
+            e.setDirection(dir);
         }
         else{
             return false;
