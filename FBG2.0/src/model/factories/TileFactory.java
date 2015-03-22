6500e0bc747b219b87367ee144ab75bb8170cc67
hanif <hanif314@gmail.com>
2015-03-22 16:39:43 -0400

    removed unneccesary stuff from tile

--- FBG2.0/src/model/factories/TileFactory.java
+++ FBG2.0/src/model/factories/TileFactory.java
@@ -1,10 +1,33 @@
 
 package model.factories;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.w3c.dom.Element;
+import org.w3c.dom.NodeList;
+
+import model.gameObject.MapObject;
+import model.map.tile.*;
+
 /**
  *
- * @author ChrisMoscoso
+ * This class parses a map file for all the tiles and instantiates them
+ *
+ * @author Hanif 
+ * 
+ * This class 
  */
 public class TileFactory {
-    
+    public List<Tile> generate(Element head)
+    {
+    	List<Tile> items = new ArrayList<Tile>();
+    	
+    	NodeList nodes = head.getElementsByTagName("tile");
+    	
+    	
+    	
+		return null;
+    	
+    }
 }
--- FBG2.0/src/model/map/tile/Tile.java
+++ FBG2.0/src/model/map/tile/Tile.java
@@ -16,25 +16,21 @@ import model.entity.MotionType;
 public class Tile {
 
 	Terrain terrain;
-	AreaEffect effect;
-	Trap tileTrap;
-	Decal decal;
+	
 	
         /**
          * By default return an empty tile of default terrain
          */
 	public Tile(){
-            this(new Terrain(), null, null, null );
+            this(new Terrain());
 	}
 	
         /**
          * 
          */
-	public Tile(Terrain t, AreaEffect e, Trap tr, Decal d){
+	public Tile(Terrain t){
 		terrain = t;
-		effect = e;
-		tileTrap = tr;
-		decal = d;
+	
 	}
 	
         /**
@@ -67,103 +63,8 @@ public class Tile {
 	public void removeTerrain(){
 		terrain = null;
 	}
-	
-	/**
-	 * Adds a decal to this tile
-	 * 
-	 * @author Michael Cohen
-	 * @param d the decal to add to the Tile
-	 */
-	public void addDecal(Decal d){
-		if (decal != null)
-			throw new RuntimeException("This tile already has a decal");
-		else 
-			decal = d;
-	}
-	
-	/**
-	 * Removes the trap from this tile
-	 * 
-	 * @author Michael Cohen
-	 */
-	public void removeDecal(){
-		decal = null;
-	}
-	
-	/**
-	 * Adds a trap to this tile
-	 * 
-	 * @author Michael Cohen
-	 * @param t the trap to add to the tile
-	 */
-	public void addTrap(Trap t){
-		if (tileTrap != null)
-			throw new RuntimeException("This tile already has a Trap");
-		else
-			tileTrap = t;
-	}
-	
-	/**
-	 * Removes the trap from this tile
-	 * 
-	 * @author Michael Cohen
-	 */
-	public void removeTrap(){
-		tileTrap = null;
-	}
-	
-	/**
-	 * Attempts to activate Trap. Returns true if successful
-	 * 
-	 * @author Michael Cohen
-	 * @param caller Entity to apply the Trap on
-	 * @return true if Tile has an Trap to activate, otherwise false
-	 */
-	public boolean activateTrap(Entity caller){
-		if (tileTrap != null){
-			tileTrap.activate(caller);
-			return true;
-		}
-		return false;
-	}
-	
-	/**
-	 * Adds an AreaEffect to this Tile
-	 * 
-	 * @author Michael Cohen
-	 * @param e the AreaEffect to add to the Tile
-	 */
-	public void addAreaEffect(AreaEffect e){
-		if (effect != null)
-			throw new RuntimeException("This tile already has an AreaEffect");
-		else
-			effect = e;
-	}
-	
-	/**
-	 * Removes the trap from this tile
-	 * 
-	 * @author Michael Cohen
-	 */
-	public void removeAreaEffect(){
-		effect = null;
-	}
-	
-	/**
-	 * Attempts to activate AreaEffect. Returns true if successful
-	 * 
-	 * @author Michael Cohen
-	 * @param caller Entity to apply the effect on
-	 * @return true if Tile has an AreaEffect to activate, otherwise false
-	 */
-	public boolean activateEffect(Entity caller){
-		if (effect != null){
-			effect.activate(caller);
-			return true;
-		}
-		return false;
-	}
-	
+
+
 	
 	
 	
