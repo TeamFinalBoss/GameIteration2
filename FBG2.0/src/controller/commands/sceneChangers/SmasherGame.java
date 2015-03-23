8556f7c23b025377d68e974e4ff1cb33985c0ca6
kkyrazis <kkyrazis@ufl.edu>
2015-03-23 07:16:15 -0400

    New game scene should be done

--- FBG2.0/src/controller/commands/sceneChangers/SmasherGame.java
+++ FBG2.0/src/controller/commands/sceneChangers/SmasherGame.java
@@ -1,5 +1,7 @@
 package controller.commands.sceneChangers;
 
+import java.io.File;
+
 import model.director.GameDirector;
 import controller.commands.Commandable;
 import controller.sceneControllers.SceneType;
@@ -8,7 +10,7 @@ public class SmasherGame extends SceneChangerCommands implements Commandable {
 
 	@Override
 	public void execute() {
-		GameDirector.getGameDirector().startNewGame(def);
+		GameDirector.getGameDirector().startNewGame(new File("./src/resources/saves/defaultSmasher.xml"));
 		super.switchScene(SceneType.GAME);
 
 	}
