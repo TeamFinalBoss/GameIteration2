155381d19c4ecad3da02904947b1696bcf1b0e11
Aaron Iglesias <aaroniglesias@gmail.com>
2015-03-23 02:55:28 -0400

    added boon

---
+++ FBG2.0/src/model/entity/ability/HealAbility.java
@@ -0,0 +1,80 @@
+package model.entity.ability;
+
+import model.effect.Effect;
+import model.entity.Entity;
+
+import model.map.Direction;
+import model.director.CombatCoordinator;
+import model.map.pair.CoordinatePair;
+import java.util.ArrayList;
+import model.entity.Entity;
+import java.lang.Math.*;
+import java.util.List;
+import model.director.ActiveMapManager;
+import model.effect.DealDamageEffect;
+import model.effect.HealEffect;
+import model.effect.SnareEffect;
+import model.entity.ability.ProjectileAbility;
+import model.map.GameMap;
+import model.map.Locations;
+import model.map.Vector;
+import model.map.pair.PreciseCoordinatePair;
+
+/**
+*
+* @author Aaron Iglesias
+*/
+
+public class HealAbility extends RadialAbility
+{
+    private String name;
+    private HealEffect effect;
+    private CombatCoordinator myCC;
+    private ActiveMapManager myMM;
+    private Effect cost;
+	private double degree;
+	private double radius;
+
+	public HealAbility()
+	{
+		this.name = "Heal";
+		this.degree = 360;
+		this.radius = 0;
+		this.myMM = ActiveMapManager.getInstance();
+        this.effect = new HealEffect(10);
+	}
+
+	public HealAbility(String name, Effect effect, Effect cost, int degree, double radius)
+	{
+		super(name, effect, cost, degree, radius);
+		this.myCC = CombatCoordinator.getInstance();
+		this.myMM = ActiveMapManager.getInstance();
+	}
+
+	@Override
+	public boolean meetsStatRequirements(Entity summoner)
+	{
+		if(summoner.getIntellect() >= 5)
+            return true;
+        else
+            return false;
+	}
+
+	@Override
+    public boolean performAbility(Entity summoner) 
+    {
+    	GameMap map = myMM.getActiveMap();
+    	int mana = summoner.getCurrentMP();
+    	List<Entity> entities = map.getEntities();
+
+    	if(mana >= 1)
+    	{
+    		summoner.setCurrentMP(--mana);
+            this.effect.applyEffect(summoner);
+    		
+    		return true;
+    	}
+    	else
+    		return false;
+    }
+}
