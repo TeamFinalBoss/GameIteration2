e0c13fbc62f2170406b0b755593ce846046f904a
hanif <hanif314@gmail.com>
2015-03-23 04:32:54 -0400

    still working on tile factory

--- FBG2.0/src/model/factories/TileFactory.java
+++ FBG2.0/src/model/factories/TileFactory.java
@@ -8,6 +8,7 @@ import org.w3c.dom.Element;
 import org.w3c.dom.NodeList;
 
 import model.gameObject.MapObject;
+import model.map.pair.CoordinatePair;
 import model.map.tile.*;
 
 /**
@@ -28,20 +29,23 @@ public class TileFactory {
     	height = Integer.parseInt(head.getAttribute("height"));
     	
     	tiles = new Tile[height][width];
+   
     	
     	NodeList nodes = head.getElementsByTagName("tile"); 
     	int count = 0;
-    	
+    	System.out.println("Length: " + nodes.getLength());
     	for(int i = 0; i < height; ++i)
     	{
     		for(int j = 0; j < width; ++j)
     		{
     			String id = nodes.item(count++).getAttributes().item(0).getTextContent();
-    			//System.out.println("id: " + id);
+    			//id = nodes.item(count++).getAttributes().item(0).getAttributes().toString();
+    			//System.out.println("count:" + count +  "id:"  + id);
     			//TODO: change when id's have a meaning corresponding to a Terrain
-    		
+    			//id = "1";
     			tiles[i][j] = new Tile(new Terrain());
     			tiles[i][j].setID(id);
+    			tiles[i][j].setLocation(new CoordinatePair(i , j));
     			
     		}
     	}
