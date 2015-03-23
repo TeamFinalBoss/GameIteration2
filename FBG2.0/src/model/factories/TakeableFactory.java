02c50e780cd73955c81256dbb7c263c3949af3c1
almcaeobtac <almcaeobtac@gmail.com>
2015-03-23 06:28:39 -0400

    change

--- FBG2.0/src/model/factories/TakeableFactory.java
+++ FBG2.0/src/model/factories/TakeableFactory.java
@@ -11,6 +11,7 @@ import model.item.Gun;
 import model.item.Halo;
 import model.item.HealthPotion;
 import model.item.HermesBoots;
+import model.item.Hornes;
 import model.item.Item;
 import model.item.JesusBoots;
 import model.item.Mace;
