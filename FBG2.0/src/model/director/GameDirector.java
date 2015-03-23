084c7f24ad44baa92b4c1a947e6044960c643c6b
almcaeobtac <almcaeobtac@gmail.com>
2015-03-23 00:41:50 -0400

    got saving to work right

--- FBG2.0/src/model/director/GameDirector.java
+++ FBG2.0/src/model/director/GameDirector.java
@@ -170,6 +170,7 @@ public class GameDirector extends Observable implements SceneObserver {
     	MapInstantiator.getInstance().loadFullGame(def);
     	AvatarInteractionManager.getInstance().setAvatar(MapInstantiator.getInstance().createAvatarFromFile(def));
     	
+    	
     	gameScene.clearScene();
     	
        
