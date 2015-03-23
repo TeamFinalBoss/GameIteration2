2efc8b722fc459f910f361f1a439d50f01db0b08
Matthew Kroeze <matthew.kroeze@gmail.com>
2015-03-23 06:33:50 -0400

    equip removes from sack

--- FBG2.0/src/model/entity/Entity.java
+++ FBG2.0/src/model/entity/Entity.java
@@ -167,6 +167,7 @@ public abstract class Entity extends MapObject {
 
     public void equip(Equipable next) {
         myInventory.equip(next);
+        remove(next);
     }
 
     public Equipable unequip(EquipSlot slot) {
